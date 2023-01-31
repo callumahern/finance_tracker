ddc671e96eaae23e9f382f4050319162a405cb24
callumahern <callum.ahern@simplybusiness.co.uk>
2023-01-31 12:15:25 +0000

    log out functionality

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -26,6 +26,7 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
   config.navigational_formats = ['*/*', :html, :turbo_stream]
+  config.sign_out_via = :get
 
 
   # Configure the class responsible to send e-mails.
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@
 
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+ end
+ 
   root to: 'home#homepage'
 end
