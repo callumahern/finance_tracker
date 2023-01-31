22517e948aaf22a51af07a1d5ee12c1172611d36
callumahern <callum.ahern@simplybusiness.co.uk>
2023-01-31 12:01:44 +0000

    category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_29_162946) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_31_120056) do
+  create_table "categories", force: :cascade do |t|
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
